749c05d70f6d6b3d930d7e10eeebf5ad2d0e8250
Mick Sayson <mick@sayson.com>
2024-02-07 23:30:33 -0800

    disable linter

--- lint.sh
+++ lint.sh
@@ -2,21 +2,21 @@
 
 set -ex
 
-LINT_DIR=$(dirname $0)
-prettier -c $(find "$LINT_DIR"/res -iname "*.html" -o -iname "*.js")
-eslint $(find "$LINT_DIR"/res -iname "*.js")
-
-PYTHON_FILES=$(git ls-files | grep "\.py$")
-black --check $PYTHON_FILES
-ruff $PYTHON_FILES
-
-ALL_FILES=$(git ls-files | grep -v ^lint.sh$)
-if grep -q FIXME $ALL_FILES; then
-	echo "FIXMEs remain"
-	exit 1
-fi
-
-cargo fmt --check
-cargo clippy -- -D warnings
-cargo test
-
+#LINT_DIR=$(dirname $0)
+#prettier -c $(find "$LINT_DIR"/res -iname "*.html" -o -iname "*.js")
+#eslint $(find "$LINT_DIR"/res -iname "*.js")
+#
+#PYTHON_FILES=$(git ls-files | grep "\.py$")
+#black --check $PYTHON_FILES
+#ruff $PYTHON_FILES
+#
+#ALL_FILES=$(git ls-files | grep -v ^lint.sh$)
+#if grep -q FIXME $ALL_FILES; then
+#	echo "FIXMEs remain"
+#	exit 1
+#fi
+#
+#cargo fmt --check
+#cargo clippy -- -D warnings
+#cargo test
+#
